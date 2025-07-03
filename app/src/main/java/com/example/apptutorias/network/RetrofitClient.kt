82693c20f2caca275ab4d6337479a366a6369da9
vilma <dmirandao@uamv.edu.ni>
2025-07-02 23:05:09 -0600

    Le agregué para que se pueda usar con un teléfono normal y con el emulador

--- app/src/main/java/com/example/apptutorias/network/RetrofitClient.kt
+++ app/src/main/java/com/example/apptutorias/network/RetrofitClient.kt
@@ -6,7 +6,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8181/api/"
+
+    private val BASE_URL: String
+        get() = if (
+            android.os.Build.MODEL.contains("sdk_gphone", ignoreCase = true) ||
+            android.os.Build.FINGERPRINT.contains("generic", ignoreCase = true)
+        ) {
+            "http://10.0.2.2:8181/api/"
+        } else {
+            "http://192.168.1.248:8181/api/"
+        }
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
